d488d1752f2c9460193a89de3bdecd1e4a9ed0ea
Planeshifter <pburckhardt@outlook.com>
2014-12-25 12:39:54 +0100

    prepare for npm upload

--- build.sh
+++ build.sh
@@ -1,1 +1,15 @@
-emcc -O3 specfunc/*.o err/*.o sys/*.o --bind specfunc.cpp -I./ --memory-init-file 0 -o specfunc.js
+#!/bin/sh
+
+export DIR="$(pwd)"
+export GSL=$DIR/gsl-js
+
+cd $GSL
+
+emconfigure ./configure
+emmake make
+
+cd $DIR
+
+emcc -O3 $GSL/cdf/*.o $GSL/err/*.o $GSL/randist/*.o $GSL/specfunc/*.o $GSL/gsl/*.h --bind specfunc.cpp -I./ --memory-init-file 0 -o specfunc.js
+
+echo "JS build finished"
